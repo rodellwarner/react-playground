dc928faf83bfaa6b5077bbfbaaffc14c49849bd5
Rodell Warner <rodell.warner@gmail.com>
2020-09-17 15:58:33 -0500

    added lang-context

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom";
 // import Helloworld from "./state-drills/Helloworld";
 // import Bomb from "./state-drills/Bomb";
 // import RouletteGun from "./state-drills/RouletteGun";
-import { BrowserRouter } from "react-router-dom";
-import App from "./paltrows-power-toes/App";
+// import { BrowserRouter } from "react-router-dom";
+// import App from "./paltrows-power-toes/App";
+import AppLang from "./lang-context/AppLang";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  // <BrowserRouter>
+  <AppLang />,
+  // </BrowserRouter>,
   document.getElementById("root")
 );
